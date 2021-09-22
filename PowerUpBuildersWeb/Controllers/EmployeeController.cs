ca133d73093482c1a2b984521055e788553bca89
Ciuca Alexandra <maria_alex14@yahoo.com>
2021-09-22 10:53:31 +0300

    Added Employee Controller

--- PowerUpBuildersWeb/Controllers/EmployeeController.cs
+++ PowerUpBuildersWeb/Controllers/EmployeeController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PowerUpBuildersWeb.Models;
+using PowerUpBuildersWeb.Repositories;
+using PowerUpBuildersWeb.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +12,57 @@ namespace PowerUpBuildersWeb.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            EmployeeViewModel employees = new EmployeeViewModel();
+            employees.Employees = _employeeRepository.GetEmployees();
+            return View(employees);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([Bind("Id,Name,Tasks")] Employee employee)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _employeeRepository.UpdateEmployee(employee);
+                    _employeeRepository.Save();
+                }
+                catch(DbUpdateConcurrencyException)
+                {
+                    if(!_employeeRepository.GetEmployees().Contains(employee))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        public RedirectToActionResult Delete(int employeeId)
+        {
+            var employee = _employeeRepository.GetEmployees().FirstOrDefault(e => e.Id == employeeId);
+
+            if(employee != null)
+            {
+                _employeeRepository.DeleteEmployee(employeeId);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ PowerUpBuildersWeb/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PowerUpBuildersWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PowerUpBuildersWeb.ViewModel
+{
+    public class EmployeeViewModel
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+    }
+}
